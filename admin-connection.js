0ec8975ab70afdd7c724ca6aadfb04f956e17c72
acloudfan <raj@acloudfan.com>
2018-04-07 09:31:17 -0400

    ok

--- admin-connection.js
+++ admin-connection.js
@@ -18,7 +18,6 @@
  */
 
 // Need the card store instance
-//const FileSystemCardStore = require('composer-common').FileSystemCardStore;
 const AdminConnection = require('composer-admin').AdminConnection;
 
 // Used as the card for all calls
@@ -26,18 +25,11 @@ const   cardNameForPeerAdmin   = "PeerAdmin@hlfv1";
 const   cardNameForNetworkAdmin   = "admin@airlinev7";
 const   appToBePinged = "airlinev7";
 
-// 1. Create instance of file system card store
-// NOT NEEDED in 0.19.0
-//const cardStore = new FileSystemCardStore();
+// 1. Create Admin Connection object for the fabric
+var walletType = { type: 'composer-wallet-filesystem' }
+const adminConnection = new AdminConnection(walletType);
 
-// 2. Create Admin Connection object for the fabric
-// NOT NEEDED in 0.19.0
-// const cardStoreObj = { cardStore: cardStore };
-// const adminConnection = new AdminConnection(cardStoreObj);
-var cardType = { type: 'composer-wallet-filesystem' }
-const adminConnection = new AdminConnection(cardType);
-
-// 3. Initiate a connection as PeerAdmin
+// 2. Initiate a connection as PeerAdmin
 return adminConnection.connect(cardNameForPeerAdmin).then(function(){
 
     console.log("Admin Connected Successfully!!!");
@@ -50,14 +42,14 @@ return adminConnection.connect(cardNameForPeerAdmin).then(function(){
 
 // Extracts information about the network
 function listBusinessNetwork(){
-    // 4. List the network apps
+    // 3. List the network apps
     adminConnection.list().then((networks)=>{
         console.log("Successfully retrieved the deployed Networks: ",networks);
 
         networks.forEach((businessNetwork) => {
             console.log('Deployed business network', businessNetwork);
          });
-        // 5. Disconnect
+        // 4. Disconnect
         adminConnection.disconnect();
         reconnectAsNetworkAdmin();
     }).catch((error)=>{
@@ -68,14 +60,14 @@ function listBusinessNetwork(){
 // Ping the network
 function reconnectAsNetworkAdmin(){
 
-    // 6. Reconnect with the card for network admin
+    // 5. Reconnect with the card for network admin
     return adminConnection.connect(cardNameForNetworkAdmin).then(function(){
         console.log("Network Admin Connected Successfully!!!");
-        // 7. Ping the BNA 
+        // 6. Ping the BNA 
         
         adminConnection.ping(appToBePinged).then(function(response){
             console.log("Ping response from "+appToBePinged+" :",response);
-            // 8. Disconnect
+            // 7. Disconnect
             adminConnection.disconnect();
         }).catch((error)=>{
             console.log(error);
